48ec7ea050f6857cd2bd42f8b18c17dda7dd0460
Patronum213 <collinhanney@gmail.com>
2024-12-30 17:03:15 -1000

    added autonomi and (untested) joystick curving

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,7 @@
 /*    Module:       main.cpp                                                  */
 /*    Author:       VEX                                                       */
 /*    Created:      Thu Sep 26 2019                                           */
-/*    Description:  Clawbot Competition Template                              */
+/*    Description:  Competition Template                                      */
 /*                                                                            */
 /*----------------------------------------------------------------------------*/
 //testing commit
@@ -50,11 +50,20 @@ void resetMotorEncoders(void) {
   RightMotor3.resetPosition();
   ConveyorMotor.resetPosition();
 };
+//made for drive curve off, takes input of 0-1, only works positively
 float distributeNormally (float input) {
   return std::pow(2.71828, -std::pow(((4*input)-2), 2));
 };
+//made for drive curve off, takes input of 0-1, only works positively
 float distributeParabolically (float input) {
   return (-4*std::pow((input-0.5), 2) + 1);
+};
+//made for joystick curving, takes input of 0-1, works both ways
+float distributeExponentially (float input) {
+  return (input > 0 ? 
+  (std::pow(1.05, 100*input)-1)/(std::pow(1.05, 100)-1):
+  -(std::pow(1.05, std::abs(100*input))-1)/(std::pow(1.05, 100)-1)
+  );
 };
 //distance, maxSpeed, fowards
 void MoveStraight(float distance, int maxSpeed, bool fowards) {
@@ -168,7 +177,9 @@ void MoveTurning(float degrees, int maxSpeed, bool isturningright) {
   }
   
 };
-void TurnWithRatio(float distance, int maxSpeed, float LeftToRightRatio, bool fowards) {
+//distance traveled by the greater side; leftToRightRaito determines the turning direction.
+//robot turns right if it is greater than one, turns left if it is lesser
+void TurnWithRatio(float distance, int maxSpeed, double LeftToRightRatio, bool fowards) {
   resetMotorEncoders();
   //turn with ratio uses the ratio of left wheel power to right wheel power to determine direction
   //if LtoRratio is greater than 1, it turns right, less than one and it turns left
@@ -179,6 +190,7 @@ void TurnWithRatio(float distance, int maxSpeed, float LeftToRightRatio, bool fo
   float rightSideMultiplier = 1;
     if (LeftToRightRatio > 1) {
       rightSideMultiplier = 1/LeftToRightRatio;
+      if (1/LeftToRightRatio <= 0.00001) {rightSideMultiplier = 0;}
       if (fowards) {
         while (LeftMotor2.position(rev) < distancerev) {
         float distanceTraveledPct = (LeftMotor2.position(rev)/distancerev)*100.0;
@@ -312,20 +324,72 @@ void autonomous(void) {
   //6 points, preload + negative side stack + 2 center stacks; touches ladder
   //13.5 seconds
   //slot 1
-  /*MoveStraight(18, 70, false);//move out to get the goal
-  GoalPneumatics.set(false);//grab it
-  ConveyorMotor.spin(directionType::rev, 100, velocityUnits::pct);//start spinning the conveyor
-  MoveTurning(60, 50, true);//turn towards the first stack
-  MoveStraight(22, 70, true);//go in to it
-  MoveStraight(18, 50, false);// reverse to not pick up the blue ring on top
-  MoveTurning(23, 50, true);//turn towards the center stack(s)
-  MoveStraight(23, 70, true);//drive in to center facing one
+  /*GoalPneumatics.set(false);
+  MoveStraight(13, 30, false);//get in line with the wall stake, also push the ring out of the way
+  MoveTurning(90, 30, true);//turn (backwards) towards it
+  LeftMotor1.spin(directionType::rev, 30, velocityUnits::pct);//push right up against
+  LeftMotor2.spin(directionType::rev, 30, velocityUnits::pct); 
+  LeftMotor3.spin(directionType::rev, 30, velocityUnits::pct);
+  RightMotor1.spin(directionType::rev, 30, velocityUnits::pct);
+  RightMotor2.spin(directionType::rev, 30, velocityUnits::pct);
+  RightMotor3.spin(directionType::rev, 30, velocityUnits::pct);
+  wait(800, msec);
+  resetMotorEncoders();//tare encoders just becouse
+  MoveStraight(0.4, 5, true);
+  ConveyorMotor.spin(directionType::rev, 100, velocityUnits::pct);//start conveyor so it's running ambiently
+  wait(900, msec);
+  MoveStraight(7, 30, true);
+  MoveTurning(136, 30, true);//180-37
+  GoalPneumatics.set(true);
+  MoveStraight(37, 100, false);
+  GoalPneumatics.set(false);
+  MoveTurning(120, 50, true);
+  MoveStraight(24, 80, true);
+  MoveTurning(160, 80, true);
+  LeftMotor1.spin(directionType::fwd, 100, velocityUnits::pct);//push right up against
+  LeftMotor2.spin(directionType::fwd, 100, velocityUnits::pct); 
+  LeftMotor3.spin(directionType::fwd, 100, velocityUnits::pct);
+  RightMotor1.spin(directionType::fwd, 100, velocityUnits::pct);
+  RightMotor2.spin(directionType::fwd, 100, velocityUnits::pct);
+  RightMotor3.spin(directionType::fwd, 100, velocityUnits::pct);
+  wait(700, msec);
+  LeftMotor1.stop(coast);//push right up against
+  LeftMotor2.stop(coast); 
+  LeftMotor3.stop(coast);
+  RightMotor1.stop(coast);
+  RightMotor2.stop(coast);
+  RightMotor3.stop(coast);
+  */
+  /*GoalPneumatics.set(false);
+  MoveStraight(13, 45, false);//get in line with the wall stake, also push the ring out of the way
+  MoveTurning(90, 45, true);//turn (backwards) towards it
+  LeftMotor1.spin(directionType::rev, 45, velocityUnits::pct);//push right up against
+  LeftMotor2.spin(directionType::rev, 45, velocityUnits::pct); 
+  LeftMotor3.spin(directionType::rev, 45, velocityUnits::pct);
+  RightMotor1.spin(directionType::rev, 45, velocityUnits::pct);
+  RightMotor2.spin(directionType::rev, 45, velocityUnits::pct);
+  RightMotor3.spin(directionType::rev, 45, velocityUnits::pct);
+  wait(700, msec);
+  resetMotorEncoders();//tare encoders just becouse
+  MoveStraight(1.5, 10, true);
+  ConveyorMotor.spin(directionType::rev, 100, velocityUnits::pct);//start conveyor so it's running ambiently
+  wait(900, msec);
+  MoveStraight(7, 40, true);
+  MoveTurning(148, 50, true);
+  GoalPneumatics.set(true);
+  MoveStraight(30, 70, false);
+  GoalPneumatics.set(false);
+  MoveTurning(128, 60, true);
+  MoveStraight(28, 80, true);
+  MoveStraight(18, 60, false);// reverse to not pick up the blue ring on top
+  MoveTurning(23, 60, true);//turn towards the center stack(s)
+  MoveStraight(23, 80, true);//drive in to center facing one
   TurnWithRatio(38, 100, 1.0/3.0, false);//back out and align with the other
-  MoveTurning(62, 50, false);//turn towards it
-  MoveStraight(24, 70, true);//run in to it
-  MoveStraight(5, 70, false);//reverse
-  MoveTurning(75, 50, true);//turn towards the ladder
-  MoveStraight(40, 70, true);//bump in to it
+  MoveTurning(62, 60, false);//turn towards it
+  MoveStraight(24, 80, true);//run in to it
+  MoveStraight(5, 80, false);//reverse
+  MoveTurning(75, 60, true);//turn towards the ladder
+  MoveStraight(40, 80, true);//bump in to it
   */
   ////////////////////////////////////////////////////////////////////////////////
   ////////////////////////BLUE NEGATIVE SIDE 4-RING AUTO//////////////////////////
@@ -333,6 +397,42 @@ void autonomous(void) {
   //6 points, preload + negative side stack + 2 center stacks; touches ladder
   //13.5 seconds
   //slot 2
+  /*GoalPneumatics.set(false);
+  MoveStraight(13, 30, false);//get in line with the wall stake, also push the ring out of the way
+  MoveTurning(90, 30, false);//turn (backwards) towards it
+  LeftMotor1.spin(directionType::rev, 30, velocityUnits::pct);//push right up against
+  LeftMotor2.spin(directionType::rev, 30, velocityUnits::pct); 
+  LeftMotor3.spin(directionType::rev, 30, velocityUnits::pct);
+  RightMotor1.spin(directionType::rev, 30, velocityUnits::pct);
+  RightMotor2.spin(directionType::rev, 30, velocityUnits::pct);
+  RightMotor3.spin(directionType::rev, 30, velocityUnits::pct);
+  wait(800, msec);
+  resetMotorEncoders();//tare encoders just becouse
+  MoveStraight(0.4, 5, true);
+  ConveyorMotor.spin(directionType::rev, 100, velocityUnits::pct);//start conveyor so it's running ambiently
+  wait(900, msec);
+  MoveStraight(7, 30, true);
+  MoveTurning(136, 30, false);//180-37
+  GoalPneumatics.set(true);
+  MoveStraight(37, 100, false);
+  GoalPneumatics.set(false);
+  MoveTurning(120, 50, false);
+  MoveStraight(24, 80, true);
+  MoveTurning(160, 80, false);
+  LeftMotor1.spin(directionType::fwd, 100, velocityUnits::pct);//push right up against
+  LeftMotor2.spin(directionType::fwd, 100, velocityUnits::pct); 
+  LeftMotor3.spin(directionType::fwd, 100, velocityUnits::pct);
+  RightMotor1.spin(directionType::fwd, 100, velocityUnits::pct);
+  RightMotor2.spin(directionType::fwd, 100, velocityUnits::pct);
+  RightMotor3.spin(directionType::fwd, 100, velocityUnits::pct);
+  wait(700, msec);
+  LeftMotor1.stop(coast);//push right up against
+  LeftMotor2.stop(coast); 
+  LeftMotor3.stop(coast);
+  RightMotor1.stop(coast);
+  RightMotor2.stop(coast);
+  RightMotor3.stop(coast);
+  */
   /*MoveStraight(18, 70, false);//move out to get the goal
   GoalPneumatics.set(false);//grab it
   ConveyorMotor.spin(directionType::rev, 100, velocityUnits::pct);//start spinning the conveyor
@@ -349,50 +449,93 @@ void autonomous(void) {
   MoveStraight(40, 70, true);//bump in to it
   */
   ////////////////////////////////////////////////////////////////////////////////
-  /////////////////////////RED POSITIVE SIDE 3-RING AUTO//////////////////////////
+  /////////////////////////RED POSITIVE SIDE 2-RING AUTO//////////////////////////
   ////////////////////////////////////////////////////////////////////////////////
-  //5 points, preload + middle inverted stack + positive side stack
-  //11 seconds
-  //slot 4
-  /*MoveStraight(18, 70, false);//move to goal
-  GoalPneumatics.set(false);//grab it
-  ConveyorMotor.spin(directionType::rev, 100, velocityUnits::pct);
-  LobsterPneumatics.set(true);//pull the rollers up REMEMBER TO CHANGE WHEN THE ACTUATOR IS INSTALLED ///
-  MoveTurning(9, 50, true);//turn towards the center stack
-  MoveStraight(18, 30, true);//go up to/through it
-  LobsterPneumatics.set(false);//let the rollers fall
-  MoveStraight(10, 30, false);//pull it back
-  MoveTurning(155, 50, false);//turn towards the second stack
-  MoveStraight(35, 70, true);//go to it and intake the ring
-  MoveTurning(127, 70, false);//turn towards the tower
-  MoveStraight(21, 70, true);//go and touch it
+  //6 points, preload on alliance stake + middle inverted stack + positive side stack
+  //15 seconds
+  //slot 3
+  /*GoalPneumatics.set(false);
+  MoveStraight(13, 30, false);//get in line with the wall stake, also push the ring out of the way
+  MoveTurning(90, 30, false);//turn (backwards) towards it
+  LeftMotor1.spin(directionType::rev, 30, velocityUnits::pct);//push right up against
+  LeftMotor2.spin(directionType::rev, 30, velocityUnits::pct); 
+  LeftMotor3.spin(directionType::rev, 30, velocityUnits::pct);
+  RightMotor1.spin(directionType::rev, 30, velocityUnits::pct);
+  RightMotor2.spin(directionType::rev, 30, velocityUnits::pct);
+  RightMotor3.spin(directionType::rev, 30, velocityUnits::pct);
+  wait(800, msec);
+  resetMotorEncoders();//tare encoders just becouse
+  MoveStraight(0.4, 5, true);
+  ConveyorMotor.spin(directionType::rev, 100, velocityUnits::pct);//start conveyor so it's running ambiently
+  wait(900, msec);
+  MoveStraight(7, 30, true);
+  MoveTurning(136, 30, false);//180-37
+  GoalPneumatics.set(true);
+  MoveStraight(37, 100, false);
+  GoalPneumatics.set(false);
+  MoveTurning(120, 50, false);
+  MoveStraight(24, 80, true);
+  MoveTurning(160, 80, false);
+  LeftMotor1.spin(directionType::fwd, 100, velocityUnits::pct);//push right up against
+  LeftMotor2.spin(directionType::fwd, 100, velocityUnits::pct); 
+  LeftMotor3.spin(directionType::fwd, 100, velocityUnits::pct);
+  RightMotor1.spin(directionType::fwd, 100, velocityUnits::pct);
+  RightMotor2.spin(directionType::fwd, 100, velocityUnits::pct);
+  RightMotor3.spin(directionType::fwd, 100, velocityUnits::pct);
+  wait(700, msec);
+  LeftMotor1.stop(coast);//push right up against
+  LeftMotor2.stop(coast); 
+  LeftMotor3.stop(coast);
+  RightMotor1.stop(coast);
+  RightMotor2.stop(coast);
+  RightMotor3.stop(coast);
   */
   ////////////////////////////////////////////////////////////////////////////////
-  ////////////////////////BLUE POSITIVE SIDE 3-RING AUTO//////////////////////////
+  ////////////////////////BLUE POSITIVE SIDE 2-RING AUTO//////////////////////////
   ////////////////////////////////////////////////////////////////////////////////
-  //5 points, preload + middle inverted stack + positive side stack
-  //11 seconds
+  //6 points, preload on alliance stake + middle inverted stack + positive side stack
+  //15 seconds
   //slot 4
-  /*MoveStraight(18, 70, false);//move to goal
-  GoalPneumatics.set(false);//grab it
-  ConveyorMotor.spin(directionType::rev, 100, velocityUnits::pct);
-  LobsterPneumatics.set(true);//pull the rollers up REMEMBER TO CHANGE WHEN THE ACTUATOR IS INSTALLED ///
-  MoveTurning(9, 50, false);//turn towards the center stack
-  MoveStraight(18, 30, true);//go up to/through it
-  LobsterPneumatics.set(false);//let the rollers fall
-  MoveStraight(10, 30, false);//pull it back
-  MoveTurning(155, 50, true);//turn towards the second stack
-  MoveStraight(35, 70, true);//go to it and intake the ring
-  MoveTurning(127, 70, true);//turn towards the tower
-  MoveStraight(21, 70, true);//go and touch it
-  */
+  GoalPneumatics.set(false);
+  MoveStraight(13, 30, false);//get in line with the wall stake, also push the ring out of the way
+  MoveTurning(90, 30, true);//turn (backwards) towards it
+  LeftMotor1.spin(directionType::rev, 30, velocityUnits::pct);//push right up against
+  LeftMotor2.spin(directionType::rev, 30, velocityUnits::pct); 
+  LeftMotor3.spin(directionType::rev, 30, velocityUnits::pct);
+  RightMotor1.spin(directionType::rev, 30, velocityUnits::pct);
+  RightMotor2.spin(directionType::rev, 30, velocityUnits::pct);
+  RightMotor3.spin(directionType::rev, 30, velocityUnits::pct);
+  wait(800, msec);
+  resetMotorEncoders();//tare encoders just becouse
+  MoveStraight(0.4, 5, true);
+  ConveyorMotor.spin(directionType::rev, 100, velocityUnits::pct);//start conveyor so it's running ambiently
+  wait(900, msec);
+  MoveStraight(7, 30, true);
+  MoveTurning(136, 30, true);//180-37
+  GoalPneumatics.set(true);
+  MoveStraight(37, 100, false);
+  GoalPneumatics.set(false);
+  MoveTurning(120, 50, true);
+  MoveStraight(24, 80, true);
+  MoveTurning(160, 80, true);
+  LeftMotor1.spin(directionType::fwd, 100, velocityUnits::pct);//push right up against
+  LeftMotor2.spin(directionType::fwd, 100, velocityUnits::pct); 
+  LeftMotor3.spin(directionType::fwd, 100, velocityUnits::pct);
+  RightMotor1.spin(directionType::fwd, 100, velocityUnits::pct);
+  RightMotor2.spin(directionType::fwd, 100, velocityUnits::pct);
+  RightMotor3.spin(directionType::fwd, 100, velocityUnits::pct);
+  wait(700, msec);
+  LeftMotor1.stop(coast);//push right up against
+  LeftMotor2.stop(coast); 
+  LeftMotor3.stop(coast);
+  RightMotor1.stop(coast);
+  RightMotor2.stop(coast);
+  RightMotor3.stop(coast);
+  
 ////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////SKILLS///////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
-/*ArmMotor.spinToPosition(330, deg, 80.0, velocityUnits::pct, true);
-ArmMotor.spinToPosition(2, deg, 60.0, velocityUnits::pct, true);//place ring*/
-
-ArmMotor.setStopping(brake);
+/*ArmMotor.setStopping(brake);
 ConveyorMotor.spin(directionType::rev, 100, velocityUnits::pct);//start conveyor so it's running ambiently
 wait(1000, msec);
 MoveStraight(12.5, 30, true);//move foward
@@ -425,7 +568,7 @@ MoveStraight(58, 50, false);///drive over there
 ///same for other side
 ////////////////////////////////////////////
 GoalPneumatics.set(false);//grab it
-MoveStraight(8, 30, false);//drive a little further so we're at the interection*/
+MoveStraight(8, 30, false);//drive a little further so we're at the interection
 MoveTurning(85, 30, false);//turn towareds a ring
 MoveStraight(27, 60, true);//drive in to it
 MoveStraight(2, 30, false);//move back a little to make sure we're centered
@@ -445,7 +588,23 @@ MoveStraight(6, 40, false);//back up a bit
 MoveTurning(110, 50, false);//turn towards the corner
 MoveStraight(7, 70, false);//back in to it slightly
 GoalPneumatics.set(true);//release goal
-
+MoveTurning(30, 50, false);
+MoveStraight(130, 100, true);
+MoveTurning(130, 50, false);
+LeftMotor1.spin(directionType::fwd, 100, velocityUnits::pct);//push right up against
+  LeftMotor2.spin(directionType::fwd, 100, velocityUnits::pct); 
+  LeftMotor3.spin(directionType::fwd, 100, velocityUnits::pct);
+  RightMotor1.spin(directionType::fwd, 100, velocityUnits::pct);
+  RightMotor2.spin(directionType::fwd, 100, velocityUnits::pct);
+  RightMotor3.spin(directionType::fwd, 100, velocityUnits::pct);
+  wait(2000, msec);
+  LeftMotor1.stop(coast);//push right up against
+  LeftMotor2.stop(coast); 
+  LeftMotor3.stop(coast);
+  RightMotor1.stop(coast);
+  RightMotor2.stop(coast);
+  RightMotor3.stop(coast);
+*/
 }
 
 
@@ -498,22 +657,22 @@ void usercontrol(void) {
     //Driving Control
     //controller dead zone
     int deadzonepct  = 15;
-    float Axis3 = Controller1.Axis3.position(percent) > deadzonepct ? ((Controller1.Axis3.position(percent) - deadzonepct)*1.00/(100-deadzonepct))*100 : 
-    Axis3 = Controller1.Axis3.position(percent) < -deadzonepct ? ((Controller1.Axis3.position(percent) + deadzonepct)*1.00/(100-deadzonepct))*100 : 0;
-    float Axis1 = Controller1.Axis1.position(percent) > deadzonepct ? ((Controller1.Axis1.position(percent) - deadzonepct)*1.00/(100-deadzonepct))*100 : 
-    Axis1 = Controller1.Axis1.position(percent) < -deadzonepct ? ((Controller1.Axis1.position(percent) + deadzonepct)*1.00/(100-deadzonepct))*100 : 0;
-    //input power curve
-    //double curveconstant = 1.05;
-    //Axis1 = (100*(exponent(curveconstant, Axis1)-1))/(exponent(curveconstant, 100)-1);   
-    //Axis3 = (100*(exponent(curveconstant, Axis3)-1))/(exponent(curveconstant, 100)-1);   
-    
+    float Axis3 = Controller1.Axis3.position(percent);
+    float Axis1 = Controller1.Axis1.position(percent);
+    float Axis3Dead = Axis3 > deadzonepct ? ((Axis3 - deadzonepct)*1.00/(100-deadzonepct))*100 : 
+    Axis3Dead = Axis3 < -deadzonepct ? ((Axis3 + deadzonepct)*1.00/(100-deadzonepct))*100 : 0;
+    float Axis1Dead = Axis1 > deadzonepct ? ((Axis1 - deadzonepct)*1.00/(100-deadzonepct))*100 : 
+    Axis1Dead = Axis1 < -deadzonepct ? ((Axis1 + deadzonepct)*1.00/(100-deadzonepct))*100 : 0;
+    //joystick curve, taking place after deadzoning
+    float Axis1Curved = distributeExponentially(Axis1Dead/100.0)*100.0;
+    float Axis3Curved = distributeExponentially(Axis3Dead/100.0)*100.0;
 
     //sensitivity
-    Axis1 *= LRsensitivity;
-    Axis3 *= FBsensitivity;
+    Axis1Curved *= LRsensitivity;
+    Axis3Curved *= FBsensitivity;
     //set motor powers
-    leftsidepower = (Axis3 + Axis1);
-    rightsidepower = (Axis3 - Axis1);
+    leftsidepower = (Axis3Curved + Axis1Curved);
+    rightsidepower = (Axis3Curved - Axis1Curved);
     LeftMotor1.spin(directionType::fwd, leftsidepower, velocityUnits::pct); 
     LeftMotor2.spin(directionType::fwd, leftsidepower, velocityUnits::pct); 
     LeftMotor3.spin(directionType::fwd, leftsidepower, velocityUnits::pct);
